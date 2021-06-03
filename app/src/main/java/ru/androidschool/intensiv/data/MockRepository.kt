d2fa6062b0f9be3a2351d557cd00f2828de10acb
Sergey <s.dyrdasov@gaskar.group>
2021-06-03 17:24:03 +0400

    Реализация экрана отображения списка сериалов.

--- app/src/main/java/ru/androidschool/intensiv/data/MockRepository.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/MockRepository.kt
@@ -15,4 +15,18 @@ object MockRepository {
 
         return moviesList
     }
+
+    fun getTvShows(): List<TvShow> {
+
+        val tvShowList = mutableListOf<TvShow>()
+        for (x in 0..10) {
+            val tvShow = TvShow(
+                title = "Stranger things $x",
+                voteAverage = 10.0 - x
+            )
+            tvShowList.add(tvShow)
+        }
+
+        return tvShowList
+    }
 }
--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.tv_shows_fragment.*
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.MockRepository
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -14,6 +18,10 @@ class TvShowsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +38,16 @@ class TvShowsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tv_shows_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tvShowsList = MockRepository.getTvShows().map {
+                TvShowItem(it)
+            }.toList()
+
+        tv_shows_recycler_view.adapter = adapter.apply { addAll(tvShowsList) }
     }
 
     companion object {
